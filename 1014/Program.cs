cc7bc2161e103414a0f21070e8304043e73c2618
Kauet Lima <kauet123@live.com>
2022-12-11 17:07:44 -0300

    Primeiro Commit

---
+++ 1014/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distanciaTotalPercorrida, totalDeCombustivelGasto, media;
+
+            distanciaTotalPercorrida = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalDeCombustivelGasto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+
+            media = distanciaTotalPercorrida / totalDeCombustivelGasto;
+
+            Console.WriteLine(media.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+
+
+
+
+
+
+
+        }
+    }
+}
